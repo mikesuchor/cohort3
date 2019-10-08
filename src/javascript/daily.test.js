2e485299d71202e4db894a12281f9642f17b5621
Michael Suchorolski <mikesuchor@gmail.com>
2019-10-07 21:33:14 -0600

    added one test

--- src/javascript/daily.test.js
+++ src/javascript/daily.test.js
@@ -5,6 +5,6 @@ test('Compare two parameters', () => {
     expect(assertEquals("a","a")).toBe(true);
     expect(assertEquals(1,2)).toBe(false);
     expect(assertEquals(2,2)).toBe(true);
-    // expect(assertEquals("2",2)).toBe(false);
+    expect(assertEquals("2",2)).toBe(false);
     expect(assertEquals("This value", "This value")).toBe(true);
 });
