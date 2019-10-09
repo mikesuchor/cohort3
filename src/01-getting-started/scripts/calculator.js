7cd97621e2984d687a1e71737131bfdf02f10900
Michael Suchorolski <mikesuchor@gmail.com>
2019-10-09 11:05:47 -0600

    Fixed calculator

--- src/01-getting-started/scripts/calculator.js
+++ src/01-getting-started/scripts/calculator.js
@@ -1,12 +1,12 @@
-const input1 = document.querySelector("#input1");
-const input2 = document.querySelector("#input2");
-const output = document.querySelector("#output");
+const input1 = document.querySelector("#calc-input1");
+const input2 = document.querySelector("#calc-input2");
+const output = document.querySelector("#calc-output");
 
 const calculator = {
     clear: () => {
         input1.value = "";
         input2.value = "";
-        output.innerHTML = "";        
+        output.innerHTML = "__";        
     },
     add: (num1, num2) => {
         console.log(num1 + num2);
@@ -26,21 +26,21 @@ const calculator = {
     }
 }
 
-document.querySelector("#clear").addEventListener("click", calculator.clear);
+document.querySelector("#calc-clear").addEventListener("click", calculator.clear);
 
-document.querySelector("#add").addEventListener("click", () => {
+document.querySelector("#calc-add").addEventListener("click", () => {
     output.innerHTML = calculator.add(Number(input1.value), Number(input2.value));
 });
 
-document.querySelector("#subtract").addEventListener("click", () => {
+document.querySelector("#calc-subtract").addEventListener("click", () => {
     output.innerHTML = calculator.subtract(Number(input1.value), Number(input2.value));
 });
 
-document.querySelector("#multiply").addEventListener("click", () => {
+document.querySelector("#calc-multiply").addEventListener("click", () => {
     output.innerHTML = calculator.multiply(Number(input1.value), Number(input2.value));
 });
 
-document.querySelector("#divide").addEventListener("click", () => {
+document.querySelector("#calc-divide").addEventListener("click", () => {
     output.innerHTML = calculator.divide(Number(input1.value), Number(input2.value));
 });
 
