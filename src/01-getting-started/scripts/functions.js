317dd3e5494326f68361e9b92b17538e89fa6cd0
Michael Suchorolski <mikesuchor@gmail.com>
2019-10-08 19:18:49 -0600

    Minor additions for exercises

--- src/01-getting-started/scripts/functions.js
+++ src/01-getting-started/scripts/functions.js
@@ -1,10 +1,11 @@
-
 const functions = {
     
     size: (num) => {
+        if (num < 0) return "negative";
         if (num < 10) return "small";
         if (num < 20) return "medium";
-        return "large";
+        if (num <= 100) return "large";
+        return "extra large";
     },
 
     add: (num1, num2) => {
@@ -14,8 +15,6 @@ const functions = {
     subtract: (num1, num2) => {
         return num1 - num2;
     }
-
-    
 };
 
 export default functions;
