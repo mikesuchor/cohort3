1c33833d31d611064079c695a02b0ca280d0a0d4
Michael Suchorolski <mikesuchor@gmail.com>
2019-10-11 11:32:06 -0600

    Cleaned up the file

--- src/javascript/daily.js
+++ src/javascript/daily.js
@@ -1,21 +1,5 @@
-/*	
-	Write the function that will create this output:
-
-*** the two values are not the same:
-    p1--> a
-    p2--> b
-*** the two values are not the same:
-    p1--> 1
-    p2--> 2
-*** the two values are the same:
-    p1--> 2
-    p2--> 2
-*/
-
-
 const functions = {
 
-// Write the function after this comment ---
     assertEquals: (value1, value2) => {
         if(value1 === value2) return true;
         console.log("*** the two values are not the same")
@@ -24,10 +8,8 @@ const functions = {
         return false;
     },
 
-// and before this comment ---
-
     makeEmailArr: (arr) => {
-        return arr.join('.').concat("@evolveu.ca").toLowerCase()
+        return arr.join('.').concat("@evolveu.ca").toLowerCase();
     }
 }
 
