5bd507aa0b92e97c8b5b26b0d428fdc3dc73b0d2
Michael Suchorolski <mikesuchor@gmail.com>
2019-10-13 10:29:27 -0600

    Some fixes to Canadian Taxes and Calculator exercises

--- src/01-getting-started/scripts/calculator.js
+++ src/01-getting-started/scripts/calculator.js
@@ -1,12 +1,27 @@
-const input1 = document.querySelector("#calc-input1");
-const input2 = document.querySelector("#calc-input2");
-const output = document.querySelector("#calc-output");
+document.addEventListener('DOMContentLoaded', function () {
+    const input1 = document.querySelector("#calc-input1");
+    const input2 = document.querySelector("#calc-input2");
+    const output = document.querySelector("#calc-output");
+    document.querySelector("#calc-clear").addEventListener("click", calculator.clear);
+    document.querySelector("#calc-add").addEventListener("click", () => {
+        output.innerHTML = calculator.add(Number(input1.value), Number(input2.value));
+    });
+    document.querySelector("#calc-subtract").addEventListener("click", () => {
+        output.innerHTML = calculator.subtract(input1.value, input2.value);
+    });
+    document.querySelector("#calc-multiply").addEventListener("click", () => {
+        output.innerHTML = calculator.multiply(input1.value, input2.value);
+    });
+    document.querySelector("#calc-divide").addEventListener("click", () => {
+        output.innerHTML = calculator.divide(input1.value, input2.value);
+    });
+});
 
 const calculator = {
     clear: () => {
         input1.value = "";
         input2.value = "";
-        output.innerHTML = "__";        
+        output.innerHTML = "";   
     },
     add: (num1, num2) => {
         return num1 + num2;
@@ -21,23 +36,5 @@ const calculator = {
         return num1 / num2;
     }
 }
-
-document.querySelector("#calc-clear").addEventListener("click", calculator.clear);
-
-document.querySelector("#calc-add").addEventListener("click", () => {
-    output.innerHTML = calculator.add(Number(input1.value), Number(input2.value));
-});
-
-document.querySelector("#calc-subtract").addEventListener("click", () => {
-    output.innerHTML = calculator.subtract(input1.value, input2.value);
-});
-
-document.querySelector("#calc-multiply").addEventListener("click", () => {
-    output.innerHTML = calculator.multiply(input1.value, input2.value);
-});
-
-document.querySelector("#calc-divide").addEventListener("click", () => {
-    output.innerHTML = calculator.divide(input1.value, input2.value);
-});
 
 export default calculator;
--- src/01-getting-started/scripts/canadiantaxes.js
+++ src/01-getting-started/scripts/canadiantaxes.js
@@ -3,7 +3,7 @@ document.addEventListener('DOMContentLoaded', function () {
     const button = document.querySelector("#canadian-taxes-button");
     const output = document.querySelector("#canadian-taxes-output");
     button.addEventListener("click", () => {
-        output.innerHTML = `$${canadianTaxes.calculateTaxes(input.value)}`;
+        output.innerHTML = `You owe $${canadianTaxes.calculateTaxes(input.value)}`;
     });
 });
 
@@ -13,25 +13,19 @@ const canadianTaxes = {
         const incomeInBracket2 = 95259;
         const incomeInBracket3 = 147667;
         const incomeInBracket4 = 210371;
+        const maxBracket1 = () => { return incomeInBracket1 * 0.15 };
+        const maxBracket2 = () => { return (incomeInBracket2 - incomeInBracket1) * 0.205 };
+        const maxBracket3 = () => { return (incomeInBracket3 - incomeInBracket2) * 0.26 };
+        const maxBracket4 = () => { return (incomeInBracket4 - incomeInBracket3) * 0.29 };
 
         if(income > incomeInBracket4) {
-            return Math.round((((income - incomeInBracket4) * 0.33)
-            + ((incomeInBracket4 - incomeInBracket3) * 0.29)
-            + ((incomeInBracket3 - incomeInBracket2) * 0.26)
-            + ((incomeInBracket2 - incomeInBracket1) * 0.205)
-            + (incomeInBracket1 * 0.15)) * 100) / 100;
+            return Math.round(((income - incomeInBracket4) * 0.33 + maxBracket4() + maxBracket3() + maxBracket2() + maxBracket1()) * 100) / 100;
         } else if(income > incomeInBracket3) {
-            return Math.round((((income - incomeInBracket3) * 0.29)
-            + ((incomeInBracket3 - incomeInBracket2) * 0.26)
-            + ((incomeInBracket2 - incomeInBracket1) * 0.205)
-            + (incomeInBracket1 * 0.15)) * 100) / 100;
+            return Math.round(((income - incomeInBracket3) * 0.29 + maxBracket3() + maxBracket2() + maxBracket1()) * 100) / 100;
         } else if(income > incomeInBracket2) {
-            return Math.round((((income - incomeInBracket2) * 0.26)
-            + ((incomeInBracket2 - incomeInBracket1) * 0.205)
-            + (incomeInBracket1 * 0.15)) * 100) / 100;
+            return Math.round(((income - incomeInBracket2) * 0.26 + maxBracket2() + maxBracket1()) * 100) / 100;
         } else if(income > incomeInBracket1) {
-            return Math.round((((income - incomeInBracket1) * 0.205)
-            + (incomeInBracket1 * 0.15)) * 100) / 100;
+            return Math.round(((income - incomeInBracket1) * 0.205 + maxBracket1()) * 100) / 100;
         } else {
             return Math.round((income * 0.15) * 100) / 100;
         }
