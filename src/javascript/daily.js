7e42ffbdf9dd93f00b055cf941a67124f6136157
Michael Suchorolski <mikesuchor@gmail.com>
2019-10-15 21:41:40 -0600

    Completed daily exercise Prepare for Array Work - October 15

--- src/javascript/daily.js
+++ src/javascript/daily.js
@@ -12,6 +12,52 @@ const functions = {
 
     makeEmailObj: (obj) => {
         return `${obj.fname}.${obj.lname}@evolveu.ca`.toLowerCase();
+    },
+
+    prepArrayWork: {
+        forLoop: (num) => {
+            let total = 0;
+            for(let i = 0; i <= num; i++) {
+                total += i;
+            }
+            return(total);
+        },
+
+        whileLoop: (num) => {
+            let i = 0;
+            let total = 0;
+            while(i <= num) {
+                total += i;
+                i++;
+            }
+            return total;
+        },
+
+        doWhileLoop: (num) => {
+            let i = 0;
+            let total = 0;
+            do {
+                total += i;
+                i++;
+            } while (i <= num);
+            return total;
+        },
+
+        forInLoop: (obj) => {
+            let allObjects = "";
+            for (let key in obj) {
+                allObjects += `Key: ${key} Value: ${obj[key]}\n`;
+            }
+            return allObjects;
+        },
+
+        forOfLoop: (collection) => {
+            let combined = "";
+            for (let item of collection) {
+                combined += `${item} `;
+            }
+            return combined;
+        }
     }
 }
 
--- src/javascript/daily.test.js
+++ src/javascript/daily.test.js
@@ -28,3 +28,38 @@ test('email builder from an object / map', () => {
     expect(functions.makeEmailObj({ fname: "Bill", lname: "Smith" }))
         .toEqual("bill.smith@evolveu.ca");
 });
+
+test('example of for loop', () => {
+    expect(functions.prepArrayWork.forLoop(0)).toBe(0);
+    expect(functions.prepArrayWork.forLoop(5)).toBe(15);
+    expect(functions.prepArrayWork.forLoop(100)).toBe(5050);
+});
+
+test('example of while loop', () => {
+    expect(functions.prepArrayWork.whileLoop(0)).toBe(0);
+    expect(functions.prepArrayWork.whileLoop(5)).toBe(15);
+    expect(functions.prepArrayWork.whileLoop(100)).toBe(5050);
+});
+
+test('example of do while loop', () => {
+    expect(functions.prepArrayWork.doWhileLoop(0)).toBe(0);
+    expect(functions.prepArrayWork.doWhileLoop(5)).toBe(15);
+    expect(functions.prepArrayWork.doWhileLoop(100)).toBe(5050);
+});
+
+test('example of for in loop', () => {
+    const pets = {
+        dog: "Grover",
+        cat: "Fluffy"
+    };
+    expect(functions.prepArrayWork.forInLoop(pets)).toBe("Key: dog Value: Grover\nKey: cat Value: Fluffy\n");
+    expect(functions.prepArrayWork.forInLoop(0)).toBe("");
+    expect(functions.prepArrayWork.forInLoop(undefined)).toBe("");
+    expect(functions.prepArrayWork.forInLoop([])).toBe("");
+});
+
+test('example of for of loop', () => {
+    expect(functions.prepArrayWork.forOfLoop("abc")).toBe("a b c ");
+    expect(functions.prepArrayWork.forOfLoop("How are you?")).toBe("H o w   a r e   y o u ? ");
+    expect(functions.prepArrayWork.forOfLoop([0, 1, "a", "b", null])).toBe("0 1 a b null ");
+});
