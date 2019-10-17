16d3e3cc949e29d6665e7d7a1f4cd3873973bf13
Michael Suchorolski <mikesuchor@gmail.com>
2019-10-16 23:56:10 -0600

    Added card template and working buttons

--- src/02-dom/scripts/workingwithcards.js
+++ src/02-dom/scripts/workingwithcards.js
@@ -12,36 +12,56 @@ document.addEventListener('DOMContentLoaded', () => {
 const workingWithCards = {
     createCard: () => {
         const cardBody = document.createElement("div");
-        const h1 = document.createElement("h1")
-        const text = document.createTextNode(`Card${cardNumber}`);
-        const addBeforeButton = document.createElement("button");
-        const addAfterButton = document.createElement("button");
-        const addDeleteButton = document.createElement("button");
         cardBody.setAttribute("class", "card");
         cardBody.setAttribute("id", `card${cardNumber}`);
-        h1.appendChild(text);
+        const h1 = document.createElement("h1");
+        h1.appendChild(document.createTextNode(`Card${cardNumber}`));
         cardBody.appendChild(h1);
+        const addBeforeButton = document.createElement("button");
+        addBeforeButton.setAttribute("id", `add-before-button${cardNumber}`);
+        addBeforeButton.appendChild(document.createTextNode("Add Before"));
         cardBody.appendChild(addBeforeButton);
+        const addAfterButton = document.createElement("button");
+        addAfterButton.setAttribute("id", `add-after-button${cardNumber}`);
+        addAfterButton.appendChild(document.createTextNode("Add After"));
         cardBody.appendChild(addAfterButton);
+        const addDeleteButton = document.createElement("button");
+        addDeleteButton.setAttribute("id", `delete-button${cardNumber}`);
+        addDeleteButton.appendChild(document.createTextNode("Delete"));
         cardBody.appendChild(addDeleteButton);
         leftPanel.appendChild(cardBody);
+        document.querySelector(`#add-before-button${cardNumber}`).addEventListener("click", () => {
+            workingWithCards.addBefore(event);
+        });
+        document.querySelector(`#add-after-button${cardNumber}`).addEventListener("click", () => {
+            workingWithCards.addAfter(event);
+        });
+        document.querySelector(`#delete-button${cardNumber}`).addEventListener("click", () => {
+            workingWithCards.deleteCard(event);
+        });
         cardNumber++;
         return cardBody;
     },
 
-    addCard: () => {  
-    },
-
-    addBefore: () => {
+    addCard: () => {
 
     },
 
-    addAfter: () => {
-
+    addBefore: (event) => {
+        console.log("add before");
+        console.log(event.target.parentElement);
+        insertBefore(workingWithCards.createCard(), event.target.parentElement);
     },
 
-    delete: () => {
-        
+    addAfter: (event) => {
+        console.log("add after");
+        console.log(event);
+        event.target.parentElement.appendChild(workingWithCards.createCard());
+    },
+
+    deleteCard: (event) => {
+        console.log("delete card");
+        console.log(event);
     }
 }
 
--- src/02-dom/scripts/workingwithcards.test.js
+++ src/02-dom/scripts/workingwithcards.test.js
@@ -5,3 +5,15 @@ test('adding a card to the DOM', () => {
     div.setAttribute("id", "card1");
     expect(div["id"]).toBe("card1");
 });
+
+test('adding a card before selected card', () => {
+    
+});
+
+test('adding a card after selected card', () => {
+
+});
+
+test('deleting selected card', () => {
+
+});
