283d83a06062bb0dd9df8d20500fde640ec3bda9
Michael Suchorolski <mikesuchor@gmail.com>
2019-10-18 10:03:35 -0600

    Fixed variable names

--- src/01-getting-started/scripts/canadiantaxes.js
+++ src/01-getting-started/scripts/canadiantaxes.js
@@ -1,10 +1,8 @@
-document.addEventListener('DOMContentLoaded', function () {
-    const input = document.querySelector("#canadian-taxes-input");
-    const button = document.querySelector("#canadian-taxes-button");
-    const output = document.querySelector("#canadian-taxes-output");
-    button.addEventListener("click", () => {
-        output.innerHTML = `You owe $${canadianTaxes.calculateTaxes(input.value)}`;
-    });
+const input = document.querySelector("#canadian-taxes-input");
+const button = document.querySelector("#canadian-taxes-button");
+const output = document.querySelector("#canadian-taxes-output");
+button.addEventListener("click", () => {
+    output.innerHTML = `You owe $${canadianTaxes.calculateTaxes(input.value)}`;
 });
 
 const canadianTaxes = {
@@ -13,19 +11,19 @@ const canadianTaxes = {
         const incomeInBracket2 = 95259;
         const incomeInBracket3 = 147667;
         const incomeInBracket4 = 210371;
-        const maxBracket1 = () => { return incomeInBracket1 * 0.15 };
-        const maxBracket2 = () => { return (incomeInBracket2 - incomeInBracket1) * 0.205 };
-        const maxBracket3 = () => { return (incomeInBracket3 - incomeInBracket2) * 0.26 };
-        const maxBracket4 = () => { return (incomeInBracket4 - incomeInBracket3) * 0.29 };
+        const bracket1Maxed = () => { return incomeInBracket1 * 0.15 };
+        const bracked2Maxed = () => { return (incomeInBracket2 - incomeInBracket1) * 0.205 };
+        const bracket3Maxed = () => { return (incomeInBracket3 - incomeInBracket2) * 0.26 };
+        const bracket4Maxed = () => { return (incomeInBracket4 - incomeInBracket3) * 0.29 };
 
         if(income > incomeInBracket4) {
-            return Math.round(((income - incomeInBracket4) * 0.33 + maxBracket4() + maxBracket3() + maxBracket2() + maxBracket1()) * 100) / 100;
+            return Math.round(((income - incomeInBracket4) * 0.33 + bracket4Maxed() + bracket3Maxed() + bracked2Maxed() + bracket1Maxed()) * 100) / 100;
         } else if(income > incomeInBracket3) {
-            return Math.round(((income - incomeInBracket3) * 0.29 + maxBracket3() + maxBracket2() + maxBracket1()) * 100) / 100;
+            return Math.round(((income - incomeInBracket3) * 0.29 + bracket3Maxed() + bracked2Maxed() + bracket1Maxed()) * 100) / 100;
         } else if(income > incomeInBracket2) {
-            return Math.round(((income - incomeInBracket2) * 0.26 + maxBracket2() + maxBracket1()) * 100) / 100;
+            return Math.round(((income - incomeInBracket2) * 0.26 + bracked2Maxed() + bracket1Maxed()) * 100) / 100;
         } else if(income > incomeInBracket1) {
-            return Math.round(((income - incomeInBracket1) * 0.205 + maxBracket1()) * 100) / 100;
+            return Math.round(((income - incomeInBracket1) * 0.205 + bracket1Maxed()) * 100) / 100;
         } else {
             return Math.round((income * 0.15) * 100) / 100;
         }
