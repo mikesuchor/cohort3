fd635c011ab46662b5a1be5ae5dcdb4ce4622f64
Michael Suchorolski <mikesuchor@gmail.com>
2019-10-28 15:48:24 -0600

    Fixed deposit and withdraw buttons

--- src/03-objects/scripts/index.js
+++ src/03-objects/scripts/index.js
@@ -52,17 +52,16 @@ document.addEventListener("click", () => {
     if(event.target.className === "deposit-button action-button" || event.target.className === "fas fa-sign-in-alt") {
         const accountIndex = accountController.accounts.findIndex(x => x.accountName === event.target.parentNode.id);
         const inputAmount = event.target.previousSibling;
-        console.log(inputAmount.value);
-        document.getElementById(`balance${event.target.parentNode.id}`)
-            .innerText = accountController.accounts[accountIndex].deposit(Number(inputAmount.value));
+        // Refactor this
+        event.target.previousSibling.previousSibling.innerText = accountController.accounts[accountIndex].deposit(Number(inputAmount.value));
         inputAmount.value = "";
     }
 
     if(event.target.className === "withdraw-button action-button" || event.target.className === "fas fa-sign-out-alt") {
         const accountIndex = accountController.accounts.findIndex(x => x.accountName === event.target.parentNode.id);
         const inputAmount = event.target.previousSibling.previousSibling;
-        document.getElementById(`balance${event.target.parentNode.id}`)
-            .innerText = accountController.accounts[accountIndex].withdraw(inputAmount.value);
+        // Refactor this
+        event.target.previousSibling.previousSibling.previousSibling.innerText = accountController.accounts[accountIndex].withdraw(inputAmount.value);
         inputAmount.value = "";
     }
 });
