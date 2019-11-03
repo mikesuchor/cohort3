ddca5659356e114a210f7192b00bbc567bf34f78
Michael Suchorolski <mikesuchor@gmail.com>
2019-11-03 10:08:28 -0700

    Added last clicked icon functionality to complete exercise

--- react-02/src/App.js
+++ react-02/src/App.js
@@ -8,33 +8,49 @@ import shark from './shark.png'
 import trex from './trex.png'
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Hello World</h1>
-      <img src={ladybug} className="icons" alt="ladybug" />
-      <img src={lizard} className="icons" alt="lizard" />
-      <img src={parrot} className="icons" alt="parrot" />
-      <img src={peacock} className="icons" alt="peacock" />
-      <img src={shark} className="icons" alt="shark" />
-      <img src={trex} className="icons" alt="trex" />
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-      <h6>Icons by <a href="http://www.icons8.com">icons8</a></h6>
-    </div>
-  );
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      clickedIcon: "_____"
+    }
+  };
+
+  clickIcon = (icon) => {
+    this.setState({
+      clickedIcon: icon
+    });
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <h1>Hello World</h1>
+        <h2>The last clicked icon was {this.state.clickedIcon}</h2>
+        <img src={ladybug} className="icons" alt="ladybug" onClick={() => this.clickIcon("ladybug")} />
+        <img src={lizard} className="icons" alt="lizard" onClick={() => this.clickIcon("lizard")} />
+        <img src={parrot} className="icons" alt="parrot" onClick={() => this.clickIcon("parrot")} />
+        <img src={peacock} className="icons" alt="peacock" onClick={() => this.clickIcon("peacock")} />
+        <img src={shark} className="icons" alt="shark" onClick={() => this.clickIcon("shark")} />
+        <img src={trex} className="icons" alt="trex" onClick={() => this.clickIcon("trex")} />
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+        <h6>Icons by <a href="http://www.icons8.com">icons8</a></h6>
+      </div>
+    );
+  }
 }
 
 export default App;
